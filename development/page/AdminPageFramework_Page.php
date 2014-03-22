71f4421624a93e76bdb793c672039518d1ef513b
miunosoft <miunosoft@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-22 02:17:19 +0000

    Uploading v3.0.2.
    git-svn-id: https://plugins.svn.wordpress.org/admin-page-framework/trunk@879840 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- development/page/AdminPageFramework_Page.php
+++ development/page/AdminPageFramework_Page.php
@@ -376,6 +376,7 @@ abstract class AdminPageFramework_Page extends AdminPageFramework_Page_MetaBox {
 					$this->oForm->setCurrentPageSlug( $sPageSlug );
 					$this->oForm->setCurrentTabSlug( $sTabSlug );
 					$this->oForm->applyConditions();
+					$this->oForm->applyFiltersToFields( $this, $this->oProp->sClassName );	// applies filters to the conditioned field definition arrays.
 					$this->oForm->setDynamicElements( $this->oProp->aOptions );	// will update $this->oForm->aConditionedFields
 					
 					echo $oFieldsTable->getFormTables( $this->oForm->aConditionedSections, $this->oForm->aConditionedFields, array( $this, '_replyToGetSectionHeaderOutput' ), array( $this, '_replyToGetFieldOutput' ) );
@@ -419,6 +420,7 @@ abstract class AdminPageFramework_Page extends AdminPageFramework_Page_MetaBox {
 			return $this->oProp->bEnableForm 
 				? "<input type='hidden' name='page_slug' value='{$sPageSlug}' />" . PHP_EOL
 					. "<input type='hidden' name='tab_slug' value='{$sTabSlug}' />" . PHP_EOL			
+					. "<input type='hidden' name='_is_admin_page_framework' value='1' />" . PHP_EOL			
 					. "</form><!-- End Form -->"
 				: '';
 		}	
