032e9addfe790f1d2c792f6002377a5d59a6df91
miunosoft <miunosoft@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-09 07:55:52 +0000

    Tagging 3.1.2.
    git-svn-id: https://plugins.svn.wordpress.org/admin-page-framework/tags@962930 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ 3.1.2/development/_controller/AdminPageFramework_Link/AdminPageFramework_Link_PostType.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Admin Page Framework
+ * 
+ * http://en.michaeluno.jp/admin-page-framework/
+ * Copyright (c) 2013-2014 Michael Uno; Licensed MIT
+ * 
+ */
+if ( ! class_exists( 'AdminPageFramework_Link_PostType' ) ) :
+/**
+ * Provides methods for HTML link elements for custom post types.
+ *
+ * @since			2.0.0
+ * @extends			AdminPageFramework_Utility
+ * @package			AdminPageFramework
+ * @subpackage		Link
+ * @internal
+ */
+class AdminPageFramework_Link_PostType extends AdminPageFramework_Link_Base {
+	
+	/**
+	 * Stores the information to embed into the page footer.
+	 * @since			2.0.0
+	 * @remark			This is accessed from the AdminPageFramework_PostType class.
+	 */ 
+	public $aFooterInfo = array(
+		'sLeft' => '',
+		'sRight' => '',
+	);
+	
+	public function __construct( $oProp, $oMsg=null ) {
+		
+		if ( ! is_admin() ) return;
+		
+		$this->oProp = $oProp;
+		$this->oMsg = $oMsg;
+				
+		// Add script info into the footer 
+		add_filter( 'update_footer', array( $this, '_replyToAddInfoInFooterRight' ), 11 );
+		add_filter( 'admin_footer_text' , array( $this, '_replyToAddInfoInFooterLeft' ) );	
+		$this->_setFooterInfoLeft( $this->oProp->aScriptInfo, $this->aFooterInfo['sLeft'] );
+		$aLibraryData = $this->oProp->_getLibraryData();
+		$aLibraryData['sVersion'] = $this->oProp->bIsMinifiedVersion ? $aLibraryData['sVersion'] . '.min' : $aLibraryData['sVersion'];
+		$this->_setFooterInfoRight( $aLibraryData, $this->aFooterInfo['sRight'] );
+				
+		// For post type posts listing table page ( edit.php )
+		if ( isset( $_GET['post_type'] ) && $_GET['post_type'] == $this->oProp->sPostType )
+			add_action( 'get_edit_post_link', array( $this, '_replyToAddPostTypeQueryInEditPostLink' ), 10, 3 );
+		
+	}
+	
+	/**
+	 * Adds the <em>post_type</em> query key and value in the link url.
+	 * 
+	 * This is used to make it easier to detect if the linked page belongs to the post type created with this class.
+	 * So it can be used to embed footer links.
+	 * 
+	 * @since			2.0.0
+	 * @remark			e.g. http://.../wp-admin/post.php?post=180&action=edit -> http://.../wp-admin/post.php?post=180&action=edit&post_type=[...]
+	 * @remark			A callback for the <em>get_edit_post_link</em> hook.
+	 */	 
+	public function _replyToAddPostTypeQueryInEditPostLink( $sURL, $iPostID=null, $sContext=null ) {
+		return add_query_arg( array( 'post' => $iPostID, 'action' => 'edit', 'post_type' => $this->oProp->sPostType ), $sURL );	
+	}	
+
+	/**
+	 * 
+	 * @since			2.0.0
+	 * @remark			A callback for the filter hook, <em>admin_footer_text</em>.
+	 * @internal
+	 */ 
+	public function _replyToAddInfoInFooterLeft( $sLinkHTML='' ) {
+		
+		// Check if it's in the post definition page and the post listing page
+		if ( ! $this->isPostDefinitionPage( $this->oProp->sPostType ) && ! $this->isPostListingPage( $this->oProp->sPostType ) )	
+			return $sLinkHTML;	// $sLinkHTML is given by the hook.
+			
+		if ( empty( $this->oProp->aScriptInfo['sName'] ) ) return $sLinkHTML;
+					
+		return $this->aFooterInfo['sLeft'];
+		
+	}
+	/**
+	 * 
+	 * @since			2.0.0
+	 * @remark			A callback for the filter hook, <em>admin_footer_text</em>.
+	 * @internal
+	 */ 	
+	public function _replyToAddInfoInFooterRight( $sLinkHTML='' ) {
+
+		// Check if it's in the post definition page and the post listing page
+		if ( ! $this->isPostDefinitionPage( $this->oProp->sPostType ) && ! $this->isPostListingPage( $this->oProp->sPostType ) )	
+			return $sLinkHTML;	// $sLinkHTML is given by the hook.
+			
+		return $this->aFooterInfo['sRight'];		
+			
+	}
+}
+endif;
---
+++ 3.1.2/development/utility/AdminPageFramework_Debug.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Admin Page Framework
+ * 
+ * http://en.michaeluno.jp/admin-page-framework/
+ * Copyright (c) 2013-2014 Michael Uno; Licensed MIT
+ * 
+ */
+if ( ! class_exists( 'AdminPageFramework_Debug' ) ) :
+/**
+ * Provides debugging methods.
+ *
+ * @since			2.0.0
+ * @package			AdminPageFramework
+ * @subpackage		Debug
+ * @internal
+ */
+class AdminPageFramework_Debug {
+		
+	/**
+	 * Prints out the given array contents
+	 * 
+	 * If a file pass is given, it saves the output in the file.
+	 * 
+	 * @since			unknown
+	 */
+	static public function dumpArray( $asArray, $sFilePath=null ) {
+		echo self::getArray( $asArray, $sFilePath );
+	}
+	
+	/**
+	 * Retrieves the output of the given array contents.
+	 * 
+	 * If a file pass is given, it saves the output in the file.
+	 * 
+	 * @since			2.1.6			The $bEncloseInTag parameter is added.
+	 * @since			3.0.0			Changed the $bEncloseInTag parameter to bEscape.
+	 */
+	static public function getArray( $asArray, $sFilePath=null, $bEscape=true ) {
+			
+		if ( $sFilePath ) self::logArray( $asArray, $sFilePath );			
+		
+		return $bEscape
+			? "<pre class='dump-array'>" . htmlspecialchars( print_r( $asArray, true ) ) . "</pre>"	// esc_html() has a bug that breaks with complex HTML code.
+			: print_r( $asArray, true );	// non-escape is used for exporting data into file.
+		
+	}	
+	
+	/**
+	 * Logs the given array output into the given file.
+	 * 
+	 * @since			2.1.1
+	 * @since			3.0.3		Changed the default log location and file name.
+	 * @deprecated		3.1.0		Use the log() method instead
+	 */
+	static public function logArray( $asArray, $sFilePath=null ) {
+		self::log( $asArray, $sFilePath );		
+	}	
+	
+	/**
+	 * Logs the given variable output to a file.
+	 * 
+	 * @remark		The alias of the logArray() method.
+	 * @since		3.1.0
+	 **/
+	static public function log( $v, $sFilePath=null ) {
+				
+		static $_iPageLoadID;	// identifies the page load.
+		$_iPageLoadID		= $_iPageLoadID ? $_iPageLoadID : uniqid();		
+		$_oCallerInfo		= debug_backtrace();
+		$_sCallerFunction	= isset( $_oCallerInfo[ 1 ]['function'] ) ? $_oCallerInfo[ 1 ]['function'] : '';
+		$_sCallerClasss		= isset( $_oCallerInfo[ 1 ]['class'] ) ? $_oCallerInfo[ 1 ]['class'] : '';
+		$sFilePath 			= ! $sFilePath
+			? WP_CONTENT_DIR . DIRECTORY_SEPARATOR . get_class() . '_' . $_sCallerClasss . '_' . date( "Ymd" ) . '.log'
+			: ( true === $sFilePath
+				? WP_CONTENT_DIR . DIRECTORY_SEPARATOR . get_class() . '_' . date( "Ymd" ) . '.log'
+				: $sFilePath
+			);
+		$_sHeading = date( "Y/m/d H:i:s", current_time( 'timestamp' ) ) . ' ' 
+			. "{$_iPageLoadID} {$_sCallerClasss}::{$_sCallerFunction} " 
+			. current_filter() . ' '
+			. AdminPageFramework_Utility::getCurrentURL();
+		file_put_contents( 
+			$sFilePath, 
+			$_sHeading . PHP_EOL . print_r( $v, true ) . PHP_EOL . PHP_EOL,
+			FILE_APPEND 
+		);			
+	}		
+}
+endif;
